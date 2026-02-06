f5596fc9d3326d92d81d3ceacac3c2a5d140d48f
xiangteng007 <xiangteng007@gmail.com>
2026-02-07 02:31:51 +0800

    fix(modules): Add missing service providers/exports for TelegramService DI
    - FinanceModule: add AgingAnalysisService + Invoice entity
    - Both modules now properly export services used by TelegramService
    - Fixes 'Nest can't resolve dependencies' errors on startup

--- apps/api/src/modules/finance/finance.module.ts
+++ apps/api/src/modules/finance/finance.module.ts
@@ -1,13 +1,16 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Account, Transaction, Loan } from "./entities";
+import { Invoice } from "../invoices/invoice.entity";
 import { FinanceService } from "./finance.service";
 import { FinanceController } from "./finance.controller";
+import { AgingAnalysisService } from "./aging-analysis.service";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Account, Transaction, Loan])],
+  imports: [TypeOrmModule.forFeature([Account, Transaction, Loan, Invoice])],
   controllers: [FinanceController],
-  providers: [FinanceService],
-  exports: [FinanceService],
+  providers: [FinanceService, AgingAnalysisService],
+  exports: [FinanceService, AgingAnalysisService],
 })
 export class FinanceModule {}
+
