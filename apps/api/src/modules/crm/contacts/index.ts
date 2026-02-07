fb864dcfc69be48389ee4ee3026ad39e63a90524
xiangteng007 <xiangteng007@gmail.com>
2026-02-07 10:46:10 +0800

    chore: delete 38 dead code files from legacy CRM modules

--- apps/api/src/modules/crm/contacts/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./contact.entity";
-export * from "./contact.dto";
-export * from "./contacts.service";
-export * from "./contacts.module";
--- apps/api/src/modules/supply-chain/shared/index.ts
+++
@@ -1,4 +0,0 @@
-/**
- * Supply Chain Shared Module Barrel Export
- */
-export * from "./supply-chain.enums";
